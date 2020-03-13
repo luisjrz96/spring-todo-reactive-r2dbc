0fb34adc22fad5b787bb85287af53becc3106c97
luisjrz96 <luisjrz96@hotmail.com>
2020-03-12 20:00:51 -0600

    generating defualt data to tables with schema.sql file

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tasks (title, description, expected_date, completed) VALUES ('Excercise', 'Do excersice in the park', '2020-03-20', false);
-INSERT INTO tasks (title, description, expected_date, completed) VALUES ('Programming my own website', 'Create a blog to post cool topics', '2020-03-20', false);
