e5f31edc5bde4af2e942ffa77493d0eec45cab67
Luis Juarez <luisjrz96@hotmail.com>
2020-07-18 01:23:26 -0500

    adding query to check task between dates

--- src/main/java/com/luisjrz/reactive/todo/repository/TaskRepository.java
+++ src/main/java/com/luisjrz/reactive/todo/repository/TaskRepository.java
@@ -1,9 +1,17 @@
 package com.luisjrz.reactive.todo.repository;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 import com.luisjrz.reactive.todo.model.Task;
 
-public interface TaskRepository extends R2dbcRepository<Task, Long>{
+import reactor.core.publisher.Flux;
+
+public interface TaskRepository extends R2dbcRepository<Task, Long> {
+	
+	@Query("SELECT * FROM tasks t WHERE t.date_to_complete BETWEEN :startDate AND :endDate")
+	Flux<Task> findBetweenDates(LocalDateTime startDate, LocalDateTime endDate);
 
 }
