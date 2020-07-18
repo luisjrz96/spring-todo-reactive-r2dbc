7004b927a0b037fae3f965e92f8327ffc9342fc7
Luis Juarez <luisjrz96@hotmail.com>
2020-07-18 01:14:55 -0500

    updating model to be able to check by dates with date and time

--- src/main/java/com/luisjrz/reactive/todo/model/Task.java
+++ src/main/java/com/luisjrz/reactive/todo/model/Task.java
@@ -1,11 +1,12 @@
 package com.luisjrz.reactive.todo.model;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
@@ -28,10 +29,12 @@ public class Task {
 	private String title;
 	@NotBlank(message = "description is required")
 	private String description;
-	@NotNull
-	@DateTimeFormat(iso = ISO.DATE, pattern = "yyyy-MM-dd")
-	private LocalDate expectedDate;
-	private boolean completed=false;
+	@NotNull(message = "date_to_complete must not be null")
+	@DateTimeFormat(iso = ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+	@Column(value = "date_to_complete")
+	private LocalDateTime dateToComplete;
+	private boolean completed;
+
 	
 	
 }
