90a2d69cbda422ea1c8d870a3efdceda658eba20
Luis Juarez <luisjrz96@hotmail.com>
2020-07-18 02:03:28 -0500

    updating schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
-DROP TABLE if exists tasks;
-CREATE TABLE tasks (id SERIAL PRIMARY KEY AUTO_INCREMENT, title varchar(255) not null, description varchar(255) not null, expected_date timestamp not null, completed boolean);
+/* create database todo and after create the below table manually */
 
-INSERT INTO tasks (title, description, expected_date, completed) VALUES ('Excercise', 'Do excersice in the park', '2020-03-20', false);
-INSERT INTO tasks (title, description, expected_date, completed) VALUES ('Programming my own website', 'Create a blog to post cool topics', '2020-03-20', false);
+/*
+
+CREATE TABLE tasks (id SERIAL PRIMARY KEY AUTO_INCREMENT, title varchar(255) not null, description varchar(255) not null, date_to_complete timestamp not null, completed boolean);
+
+*/
